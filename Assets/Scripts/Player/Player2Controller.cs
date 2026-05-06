800358a210d9a70ce945aa94a5de389bc1754b9d
OlofPB <olof.pilhage@gmail.com>
2026-05-06 21:32:54 +0200

    Player2Controller
    Implemented raycast to identify what the player2 is interacting with and also implemented cursor (placeholder)

--- Assets/Scripts/Player/Player2Controller.cs
+++ Assets/Scripts/Player/Player2Controller.cs
@@ -6,31 +6,49 @@ public class Player2Controller : MonoBehaviour
     private InputAction leftMouseClick;
     private InputAction rightMouseClick;
 
+    [SerializeField] private Transform cursorIndicator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InputActionAsset inputActions = GetComponent<PlayerInput>().actions;
-        leftMouseClick = inputActions.FindAction("Attack");
-        rightMouseClick = inputActions.FindAction("SecondaryAttack");
+        leftMouseClick = inputActions.FindAction("PrimaryInteract");
+        rightMouseClick = inputActions.FindAction("SecondaryInteract");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 worldPosition = CheckMousePosition();
+
         if (leftMouseClick.WasPressedThisFrame())
         {
-            CheckMousePosition();
-            Debug.Log("Primary attack triggered");
+            Debug.Log($"Primary interact triggered at {worldPosition}.");
+            Interact(worldPosition);
         }
         if (rightMouseClick.WasPressedThisFrame())
         {
-            CheckMousePosition();
-            Debug.Log("Secondary attack trigered.");
+            Debug.Log($"Secondary interact trigered at {worldPosition}.");
+            Interact(worldPosition);
         }
     }
 
-    public void CheckMousePosition()
+    public Vector2 CheckMousePosition()
     {
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        cursorIndicator.position = worldPosition;
+        return worldPosition;
+    }
+
+    private void Interact(Vector2 position)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+        if (hit.collider == null)
+        {
+            Debug.Log("Nothing to interact with.");
+        } else
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
     }
 }
