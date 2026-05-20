f65574e0e35776b41db6bf2fe9da4f6da56180b3
OlofPB <olof.pilhage@gmail.com>
2026-05-20 10:58:46 +0200

    waterdroplet
    wip

---
+++ Assets/Scripts/Elements/WaterElement/WaterDroplet.cs
@@ -0,0 +1,67 @@
+using System.Linq;
+using UnityEngine;
+
+public class WaterDroplet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool hasSettled = false;
+    private WaterZone waterZone;
+
+
+    [SerializeField] private float settleThreshold = 0.2f;
+    [SerializeField] private float mergeDropletsRadius = 0.5f;
+    [SerializeField] private float dropletTimer = 10f;
+    [SerializeField] private float addWaterAmount = 1f;
+    [SerializeField] private int mergeThreshold = 5;
+
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Settle();
+
+        dropletTimer -= Time.deltaTime;
+        if (dropletTimer < 0) DestroyDroplet();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water"))
+        {
+            MergeIntoZone(collision.gameObject.GetComponent<WaterZone>());
+        }
+    }
+
+    private void Settle()
+    {
+        if (hasSettled) return;
+        if (rb.linearVelocity.magnitude < settleThreshold)
+        {
+            hasSettled = true;
+            Collider2D[] nearby = Physics2D.OverlapCircleAll(rb.position, mergeDropletsRadius);
+            int dropletCount = nearby.Count(c => c.CompareTag("Droplet"));
+            if (dropletCount >= mergeThreshold) SpawnWaterZone();
+        }
+    }
+
+    private void MergeIntoZone(WaterZone zone)
+    {
+        waterZone.AddWater(addWaterAmount);
+        Destroy(gameObject);
+    }
+
+    private void SpawnWaterZone()
+    {
+        
+    }
+
+    private void DestroyDroplet()
+    {
+        Destroy(gameObject);
+    }
+}
